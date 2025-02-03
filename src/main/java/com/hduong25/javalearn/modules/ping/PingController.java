27f86f44f29c85d5e617d3778a4d18552867c9ba
Noah <dhduong25@gmail.com>
2025-02-03 21:50:39 +0700

    hduong

--- src/main/java/com/hduong25/javalearn/modules/ping/PingController.java
+++ src/main/java/com/hduong25/javalearn/modules/ping/PingController.java
@@ -1,6 +1,8 @@
 package com.hduong25.javalearn.modules.ping;
 
+import com.hduong25.javalearn.utils.response.SuccessResponse;
 import com.hduong25.javalearn.utils.result.Result;
+import com.hduong25.javalearn.utils.result.ResultUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ping")
 public class PingController {
 
-    private final PingService pingService;
-
     @PostMapping
     public Result<String> ping() {
-        return this.pingService.pingApp();
+        return ResultUtils.success(SuccessResponse.PING_SUCCESS);
     }
 }
